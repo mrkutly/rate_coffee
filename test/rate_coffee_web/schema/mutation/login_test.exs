ac35feffc38d11f8857480d15026d32780619a32
mark sauer-utley <mark.utley.713@gmail.com>
2020-09-05 19:29:49 -0400

    add tests for login mutation

--- test/rate_coffee_web/schema/mutation/login_test.exs
+++ test/rate_coffee_web/schema/mutation/login_test.exs
@@ -1,5 +1,6 @@
 defmodule RateCoffeeWeb.Schema.Mutation.LoginTest do
   use RateCoffeeWeb.ConnCase, async: true
+  alias RateCoffee.UserManager.Auth
 
   @query """
   mutation Login($email: String!, $password: String!) {
@@ -14,14 +15,35 @@ defmodule RateCoffeeWeb.Schema.Mutation.LoginTest do
 
   test "login returns an auth token when given valid credentials" do
     user = insert(:user)
-    # conn = build_conn()
-    IO.inspect(user)
-    # conn =
-    #   post(conn, "/api",
-    #     query: @query,
-    #     variables: %{"email" => user.email, "password" => user.password}
-    #   )
+    conn = build_conn()
 
-    # assert json_response(conn, 200) == %{}
+    result = conn
+      |> post("/api",
+        query: @query,
+        variables: %{"email" => user.email, "password" => user.password}
+      )
+      |> json_response(200)
+
+     %{"data" => %{"login" => %{"token" => token, "user" => %{"email" => email}}}} = result
+      assert email == user.email
+
+      {:ok, authed, _} = Auth.resource_from_token(token)
+      assert authed == clean_password(user)
+  end
+
+  test "login returns an error when given invalid credentials" do
+    user = insert(:user)
+    conn = build_conn()
+
+    result = conn
+      |> post("/api",
+        query: @query,
+        variables: %{"email" => user.email, "password" => user.password <> "JK!"}
+      )
+      |> json_response(200)
+
+    %{"data" => %{"login" => login}, "errors" => [%{"message" => message}]} = result
+    assert is_nil(login)
+    assert message == "Incorrect email or password"
   end
 end
